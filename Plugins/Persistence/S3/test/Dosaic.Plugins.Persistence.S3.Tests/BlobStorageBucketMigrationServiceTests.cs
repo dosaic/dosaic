f6bf53e2437d83bc9136c54e3ea70cb7389c7db0
fabian bornschlegl <fb@vfm-gruppe.de>
2025-06-03 17:19:37 +0200

    fix blobstorage migration unit test

--- Plugins/Persistence/S3/test/Dosaic.Plugins.Persistence.S3.Tests/BlobStorageBucketMigrationServiceTests.cs
+++ Plugins/Persistence/S3/test/Dosaic.Plugins.Persistence.S3.Tests/BlobStorageBucketMigrationServiceTests.cs
@@ -20,12 +20,12 @@ public class BlobStorageBucketMigrationServiceTests
 
         mc.ListBucketsAsync(Arg.Any<CancellationToken>())
             .Returns(_ => throw new Exception("retry"),
-                _ => new ListAllMyBucketsResult { Buckets = [new Bucket { Name = "NOT_REQUIRED_BUCKET" }] });
+                _ => new ListAllMyBucketsResult { Buckets = [new Bucket { Name = "dev-NOT_REQUIRED_BUCKET" }] });
 
         var buckets = Enum.GetValues<SampleBucket>().Select(x => x.GetName()).ToList();
         var fakeLogger = new FakeLogger<BlobStorageBucketMigrationService<SampleBucket>>();
         var fileStorage = Substitute.For<IFileStorage>();
-        fileStorage.ResolveBucketName(Arg.Any<string>()).Returns("dev-");
+        fileStorage.ResolveBucketName(Arg.Any<string>()).Returns(info => $"dev-{info.Args()[0]}");
         var svc = new BlobStorageBucketMigrationService<SampleBucket>(mc, fakeLogger, fileStorage);
         await svc.StartAsync(CancellationToken.None);
         svc.ExecuteTask.Should().NotBeNull();
@@ -37,8 +37,9 @@ public class BlobStorageBucketMigrationServiceTests
         fakeLogger.Entries[0].Message.Should().Be("Could not migrate s3 buckets<SampleBucket> -> retrying");
 
         // fakelogger does not resolve nested lists and objects but serilog does
-        fakeLogger.Entries[1].Message.Should().Be("S3 buckets<SampleBucket { MissingBuckets = System.Collections.Generic.List`1[System.String], RequiredBuckets = System.Collections.Generic.List`1[System.String], ExistingBuckets = System.Collections.Generic.List`1[System.String] }");
-        fakeLogger.Entries[2].Message.Should().Be("S3 buckets<SampleBucket>: create missing bucket logos");
-        fakeLogger.Entries[3].Message.Should().Be("S3 buckets<SampleBucket>: create missing bucket docs");
+        fakeLogger.Entries[1].Message.Should().Be(
+            "S3 buckets<SampleBucket { MissingBuckets = System.Collections.Generic.List`1[System.String], RequiredBuckets = System.Collections.Generic.List`1[System.String], ExistingBuckets = System.Collections.Generic.List`1[System.String] }");
+        fakeLogger.Entries[2].Message.Should().Be("S3 buckets<SampleBucket>: create missing bucket dev-logos");
+        fakeLogger.Entries[3].Message.Should().Be("S3 buckets<SampleBucket>: create missing bucket dev-docs");
     }
 }
