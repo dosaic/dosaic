a63f61b6b8c3a0c68d71f8be3930278c7be4011f
fabian bornschlegl <fb@vfm-gruppe.de>
2026-03-13 10:24:22 +0100

    fix: simplify configuration section serialization by removing unused methods and enforcing YAML serialization

--- Hosting/Abstractions/src/Dosaic.Hosting.Abstractions/Extensions/ConfigurationExtensions.cs
+++ Hosting/Abstractions/src/Dosaic.Hosting.Abstractions/Extensions/ConfigurationExtensions.cs
@@ -1,10 +1,5 @@
 using System.Globalization;
-using System.Reflection;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.CommandLine;
-using Microsoft.Extensions.Configuration.EnvironmentVariables;
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.Extensions.Configuration.Memory;
 
 namespace Dosaic.Hosting.Abstractions.Extensions
 {
@@ -20,51 +15,8 @@ namespace Dosaic.Hosting.Abstractions.Extensions
         public static object GetSection(this IConfiguration configuration, string sectionKey, Type type)
         {
             var section = configuration.GetSection(sectionKey);
-
-            var serializationMethod = ResolveSerializationMethod(section);
-            var obj = ConfigurationSectionToObject(section);
-
-            return serializationMethod != null
-                ? obj.Serialize(serializationMethod.Value)
-                    .Deserialize(type, serializationMethod.Value)
-                : obj.Serialize().Deserialize(type);
-        }
-
-        internal static SerializationMethod? ResolveSerializationMethod(IConfigurationSection section)
-        {
-            var rootField = typeof(ConfigurationSection)
-                .GetField("_root", BindingFlags.NonPublic | BindingFlags.Instance);
-            var root = rootField?.GetValue(section) as IConfigurationRoot;
-
-            IConfigurationProvider configurationProvider = null;
-            if (root != null)
-            {
-                var firstChild = section.GetChildren().FirstOrDefault();
-                if (firstChild != null)
-                {
-                    configurationProvider = root.Providers
-                        .Reverse()
-                        .FirstOrDefault(p => p.TryGet(firstChild.Path, out _));
-                }
-            }
-
-            if (configurationProvider == null)
-            {
-                return null;
-            }
-
-            return configurationProvider switch
-            {
-                _ when configurationProvider.GetType().FullName ==
-                       "NetEscapades.Configuration.Yaml.YamlConfigurationProvider" =>
-                    SerializationMethod.Yaml,
-                JsonConfigurationProvider => SerializationMethod.Json,
-                EnvironmentVariablesConfigurationProvider => SerializationMethod.Json,
-                CommandLineConfigurationProvider => SerializationMethod.Json,
-                MemoryConfigurationProvider => SerializationMethod.Json,
-                _ => throw new NotSupportedException(
-                    $"Unsupported configuration provider: {configurationProvider.GetType().FullName}")
-            };
+            return ConfigurationSectionToObject(section).Serialize(SerializationMethod.Yaml)
+                .Deserialize(type, SerializationMethod.Yaml);
         }
 
         private static object ConfigurationSectionToObject(IConfigurationSection section)
