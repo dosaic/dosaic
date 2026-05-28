fa4f17367523d4d27b5a5ef91739878b73a6ea6d
fabian bornschlegl <fb@vfm-gruppe.de>
2026-05-28 11:04:35 -0500

    Add unit tests for UnitTestAppsettingsLoader and enhance configuration loading logic

--- Testing/NUnit/src/Dosaic.Testing.NUnit/UnitTestAppsettingsLoader.cs
+++ Testing/NUnit/src/Dosaic.Testing.NUnit/UnitTestAppsettingsLoader.cs
@@ -9,8 +9,9 @@ namespace Dosaic.Testing.NUnit
         public static IConfiguration LoadLocalAppsettings(string[] additionalFiles = null)
         {
             var config = new ConfigurationBuilder();
-            config.AddFile("appsettings.yaml", optional: true);
-            config.AddFile("appsettings.secrets.yaml", optional: true);
+            var appsettingFiles = FindAllRootAppsettingFiles();
+            AddConfigurationFiles(config, appsettingFiles);
+            config.AddEnvironmentVariables();
             if (additionalFiles != null)
             {
                 foreach (var file in additionalFiles)
@@ -21,7 +22,17 @@ namespace Dosaic.Testing.NUnit
             return config.Build();
         }
 
-        public static void AddFile(this ConfigurationBuilder configurationManager, string file, bool optional = true, bool reloadOnChange = true)
+        public static T LoadSection<T>(IConfiguration config = null, string[] additionalFiles = null)
+        {
+            config ??= LoadLocalAppsettings(additionalFiles);
+            var sectionName = typeof(T).GetAttribute<ConfigurationAttribute>().Section;
+            var section = config.GetSection(sectionName);
+            var result = section.Get<T>() ?? throw new InvalidOperationException($"Section '{sectionName}' not found in configuration.");
+            return result;
+
+        }
+
+        private static void AddFile(this ConfigurationBuilder configurationManager, string file, bool optional = true, bool reloadOnChange = true)
         {
             if (file.EndsWith(".json"))
                 configurationManager.AddJsonFile(file, optional, reloadOnChange);
@@ -29,15 +40,40 @@ namespace Dosaic.Testing.NUnit
                 configurationManager.AddYamlFile(file, optional, reloadOnChange);
         }
 
-        public static T LoadSection<T>(string[] additionalFiles = null)
+
+
+        private static List<string> FindAllRootAppsettingFiles()
         {
-            var config = LoadLocalAppsettings(additionalFiles);
-            var sectionName = typeof(T).GetAttribute<ConfigurationAttribute>().Section;
-            var section = config.GetSection(sectionName);
-            var result = section.Get<T>();
-            if (result == null)
-                throw new InvalidOperationException($"Section '{sectionName}' not found in configuration.");
-            return result;
+            return FindAppSettingFiles("json", "yaml", "yml").ToList();
+        }
+
+        private static void AddConfigurationFiles(ConfigurationBuilder configurationManager, List<string> allSettings)
+        {
+            var orderedSettings = allSettings
+                .Distinct()
+                .OrderBy(x => Path.GetFileName(x).Split('.').Length)
+                .ToList();
+
+            foreach (var file in orderedSettings.Where(x => !IsSecretsFile(x)))
+                configurationManager.AddFile(file);
+
+            foreach (var file in orderedSettings.Where(IsSecretsFile))
+                configurationManager.AddFile(file);
+        }
+
+        private static bool IsSecretsFile(string filename) => filename.EndsWith(".secrets.yaml") ||
+                                                              filename.EndsWith(".secrets.yml") ||
+                                                              filename.EndsWith(".secrets.json");
+
+        private static IEnumerable<string> FindAppSettingFiles(params string[] extensions)
+        {
+            var files = Directory.GetFiles(Environment.CurrentDirectory)
+                .Select(x => x.Split(Path.DirectorySeparatorChar).Last())
+                .Where(x => !string.IsNullOrEmpty(x) && x.StartsWith("appsettings.", StringComparison.InvariantCulture))
+                .Where(x => extensions.Any(e => x.EndsWith(e, StringComparison.InvariantCultureIgnoreCase)))
+                .OrderBy(x => x.Split('.').Length);
+
+            return files;
         }
     }
 }
